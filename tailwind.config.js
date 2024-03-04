3c96b88148aa3c16ac9e73adc37d39678e2f80b5
RafaelFatkulin <armistman@vk.com>
2024-03-04 19:01:30 +0500

    [init]

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+  content: ['./resources/**/*.edge', './resources/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '992px',
+      xl: '1440px',
+    },
+    container: {
+      center: true,
+      padding: '.75rem',
+      screens: {
+        '2xl': '1424px',
+      },
+    },
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: 'hsl(var(--primary))',
+        secondary: 'hsl(var(--secondary))',
+        black: 'hsl(var(--black))',
+        white: 'hsl(var(--white))',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        text: 'hsl(var(--text))',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
+  },
+}
