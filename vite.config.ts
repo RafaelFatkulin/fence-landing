2d88b0d68b0a2bcb8a81223a61c248b5fa24d565
RafaelFatkulin <armistman@vk.com>
2024-03-15 16:58:11 +0500

    preparing to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import adonisjs from '@adonisjs/vite/client'
 
 export default defineConfig({
   build: {
     manifest: true,
+    assetsDir: 'resources/images/',
   },
   plugins: [
-    splitVendorChunkPlugin(),
+    // splitVendorChunkPlugin(),
     adonisjs({
       /**
        * Entrypoints of your application. Each entrypoint will
@@ -14,10 +15,12 @@ export default defineConfig({
        */
       entrypoints: [
         'resources/css/app.css',
+        'resources/js/app.js',
         'resources/js/client/app.js',
         'resources/js/admin/app.js',
-        'resources/images/hero.webp',
       ],
+
+      assetsUrl: '/assets/',
 
       /**
        * Paths to watch and reload the browser on file change
