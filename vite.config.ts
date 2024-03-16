5b42195b5f8c845c3f05cc9aa220ecb3c533d991
RafaelFatkulin <armistman@vk.com>
2024-03-17 03:37:14 +0500

    [fix]

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,6 @@ export default defineConfig({
        * Paths to watch and reload the browser on file change
        */
       reload: ['resources/views/**/*.edge'],
-      assetsUrl: '/assets',
     }),
   ],
 })
